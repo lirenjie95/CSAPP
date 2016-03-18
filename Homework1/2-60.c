96be194f168f30cc9f2424c2bfe553cf4bf41ca8
Renjie Li <renjie_li@outlook.com>
2016-03-18 22:29:56 +0800

    Homework 1

--- Homework1/2-60.c
+++ Homework1/2-60.c
@@ -1,3 +1,16 @@
-unsigned replace_byte (unsigned x, usigned char b, int i)
+#include <stdio.h>
+unsigned replace_byte (unsigned x, unsigned char b, int i)
 {
+	unsigned y,z;
+	y = 0xFF<<(i<<3);
+	y = ~y;
+	z = b<<(i<<3);
+	return x&y|z;
+}
+int main()
+{
+	unsigned A,B;
+	A = replace_byte(0x12345678,0xAB,2);
+	B = replace_byte(0x12345678,0xAB,0);
+	printf("A=%x, B=%x.\n",A,B);
 }
--- Homework1/2-73.c
+++ Homework1/2-73.c
@@ -1,3 +1,18 @@
+#include <stdio.h>
+#define TMax 0x7FFFFFFF
+#define TMin 0x80000000
 int saturating_add(int x, int y)
 {
+	int Ans;
+	Ans = x+y;
+	if(x>=0&&y>=0&&Ans<0) return TMax;
+	if(x<0&&y<0&&Ans>=0) return TMin;
+	return Ans;
+}
+int main()
+{
+	int x,y;
+	scanf("%x %x",&x,&y);
+	printf("%x\n",saturating_add(x,y));
+	return 0;
 }
