fba576c843714089eac7bd1cbe5bf01291946846
Renjie Li <renjie_li@outlook.com>
2016-04-27 11:00:07 +0800

    raw material

---
+++ Lab3/config.h
@@ -0,0 +1,32 @@
+/*********************************************************
+ * config.h - Configuration data for the driver.c program.
+ *********************************************************/
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+/* 
+ * CPEs for the baseline (naive) version of the rotate function that
+ * was handed out to the students. Rd is the measured CPE for a dxd
+ * image. Run the driver.c program on your system to get these
+ * numbers.  
+ */
+#define R64    14.7
+#define R128   17.5
+#define R256   51.4
+#define R512   138.5
+#define R1024  165.8
+
+/* 
+ * CPEs for the baseline (naive) version of the smooth function that
+ * was handed out to the students. Sd is the measure CPE for a dxd
+ * image. Run the driver.c program on your system to get these
+ * numbers.  
+ */
+#define S32   472.9
+#define S64   474.4
+#define S128  478.8
+#define S256  506.3
+#define S512  507.0
+
+
+#endif /* _CONFIG_H_ */
