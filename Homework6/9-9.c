ead46e57a953bb4a2fa1d677c9fee45f85e89b36
Renjie Li <renjie_li@outlook.com>
2016-05-28 21:59:28 +0800

    Exercise problems

---
+++ Homework6/9-9.c
@@ -0,0 +1,15 @@
+static void place(void *bp, size_t asize)
+{
+	size_t csize = GET_SIZE(HDRP(bp));
+	if ((csize - asize) >= (2*DSIZE)) {
+		PUT(HDRP(bp), PACK(asize, 1));
+		PUT(FTRP(bp), PACK(asize, 1));
+		bp = NEXT_BLKP(bp);
+		PUT(HDRP(bp), PACK(csize-asize, 0));
+		PUT(FTRP(bp), PACK(csize-asize, 0));
+	}
+	else {
+		PUT(HDRP(bp), PACK(csize, 1));
+		PUT(FTRP(bp), PACK(csize, 1));
+	}
+}
