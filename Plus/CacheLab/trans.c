3aa3a97f4b1f041b3b78e48e0794af299d90ec58
lirenjie95 <rjli13@fudan.edu.cn>
2017-03-04 20:14:59 +0800

    CacheLab PartB finished. 20.3/27

--- Plus/CacheLab/trans.c
+++ Plus/CacheLab/trans.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * trans.c - Matrix transpose B = A^T
  *
  * Each transpose function must have a prototype of the form:
@@ -6,44 +6,68 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
 #include <stdio.h>
 #include "cachelab.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-/* 
+/*
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
  *     the description string "Transpose submission", as the driver
  *     searches for that string to identify the transpose function to
- *     be graded. 
+ *     be graded.
  */
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+	int i,j,tmp,index,step;
+	int row_Block,col_Block;
+    if(M==32)
+		step = 8;
+	else if(M==64)
+		step = 4;
+	else
+		step = 16;
+	//separate the the 32X32 block into 8X8 , decrease the number of misses
+	for(row_Block = 0;row_Block < N ;row_Block+=step){
+		for(col_Block =0 ;col_Block < M; col_Block+=step){
+			for(i=row_Block ; i<row_Block+step && i<N;i++){
+				for(j=col_Block;j<col_Block+step &&j<M;j++){
+					if(i!=j){
+						B[j][i] = A[i][j];
+					}else{
+						tmp = A[i][j];//i==j means is the diagonal. if we set B right now ,the  misses and evictions will increase . because the cache set of B is same to A.
+						index = i;
+					}
+				}
+				if(col_Block == row_Block){             //just set B on the diagonal. other than shouldn't set the B
+					B[index][index] = tmp;
+				}
+			}
+		}
+	}
 }
 
-/* 
+/*
  * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
+ * a simple one below to help you get started.
+ */
 
-/* 
+/*
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
 char trans_desc[] = "Simple row-wise scan transpose";
 void trans(int M, int N, int A[N][M], int B[M][N])
 {
-    int i, j, tmp;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }    
-
+	int i, j, tmp;
+	for (i = 0; i < N; i++){
+        	for (j = 0; j < M; j++){
+                	tmp = A[i][j];
+                	B[j][i] = tmp;
+        	}
+	}
 }
 
 /*
@@ -55,12 +79,10 @@ void trans(int M, int N, int A[N][M], int B[M][N])
  */
 void registerFunctions()
 {
-    /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
-
-    /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-
+	/* Register your solution function */
+	registerTransFunction(transpose_submit, transpose_submit_desc);
+	/* Register any additional transpose functions */
+	registerTransFunction(trans, trans_desc);
 }
 
 /* 
@@ -70,15 +92,13 @@ void registerFunctions()
  */
 int is_transpose(int M, int N, int A[N][M], int B[M][N])
 {
-    int i, j;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
-                return 0;
-            }
-        }
-    }
-    return 1;
+	int i, j;
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < M; ++j) {
+			if (A[i][j] != B[j][i]) {
+				return 0;
+			}
+		}
+	}
+	return 1;
 }
-
